f93287d391393ea5c822a17318c040622651fbf2
thefinalboss01 <anishtamang247@gmail.com>
2022-05-26 13:56:29 +0545

    added some if lines

--- sum.py
+++ sum.py
@@ -1,3 +1,8 @@
 a = 1
 b = 2
-print (a + b)
+print(a + b)
+num = input("enter any number")
+if num % 2 == 0:
+    print("Number you have entered is even")
+else:
+    print("number you have entered is odd")
